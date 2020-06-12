d891eebc9afceccc9b38c8c74a8fcbc001e34b71
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2020-06-12 09:23:54 +0200

    Add release option
    If release is set, just copy the output folder in output-<release>

--- runBenchmark.py
+++ runBenchmark.py
@@ -3,19 +3,20 @@
 import os
 import click
 import shutil
+import colorama
 
 # -----------------------------------------------------------------------------
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('-t', '--test', default="all", help="Name of the test, if all run all benchmarks")
-@click.option('-r', '--release', default="9.0", help="Run the test for this release")
+@click.option('-r', '--release', default=None, help="Run the test for this release")
 def runTests_click(test, release, **kwargs):
     '''
     Run the benchmarks available in the current folder
     '''
     runTests(test, release)
 
-def runTests(test, release):
+def runTests(test, release=None):
 
     #Look for the tests
     testFolders = [ ]
@@ -28,6 +29,17 @@ def runTests(test, release):
     command = 'which Gate'
     os.system(command)
 
+    if release is not None:
+        print(colorama.Fore.RED + 'Are you sure to use Gate release: ' + str(release) + ' ? (y/n) ' + colorama.Style.RESET_ALL)
+        while(True):
+            choice = input().lower()
+            if choice == "y":
+               break
+            elif choice == "n":
+               return
+            else:
+               print("Please respond with 'y' or 'n'")
+
     #Go inside folders and run the tests:
     for testFolder in testFolders:
         os.chdir(testFolder)
@@ -36,10 +48,11 @@ def runTests(test, release):
         os.makedirs("output")
         command = 'bash ./runTest.sh'
         os.system(command)
+        if release is not None:
+            shutil.move("output", "output-" + str(release))
         os.chdir(currentDirectory)
 
     #Go inside folders and run the analysis:
-    currentDirectory = os.getcwd()
     for testFolder in testFolders:
         os.chdir(testFolder)
         command = 'python ./runAnalysis.py output*'
