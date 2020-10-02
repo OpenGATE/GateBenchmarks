c4eed4dc7237ac7fff6d161734b957eb8d7ed91a
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2020-10-02 15:16:56 +0200

    Add t5 tests Pet test

---
+++ t5/runTest.sh
@@ -0,0 +1,16 @@
+
+gate_split_and_run.py mac/main.mac --splittime -j 5 -o output
+
+#Wait Gate pids
+processId=($(ps -ef | grep 'Gate' | grep -v 'grep' | awk '{ print $2 }'))
+for pid in ${processId[*]}; do
+  while [[ ${?} == 0 ]]; do
+    sleep 1s
+    ps -p $pid >/dev/null
+  done
+done
+
+gate_power_merge.sh output
+rm -rf output
+mv results output
+
