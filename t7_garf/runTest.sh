d3e042294cc3679bb588b0c9600bdf22957bd438
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-02-12 11:24:36 +0100

    Problem with gate_split_and_run script, run Gate directly

--- t7_garf/runTest.sh
+++ t7_garf/runTest.sh
@@ -1,20 +1,3 @@
 
-gate_split_and_run.py mac/main_arf_v2.mac -a RADIONUCLIDE Lu177 -a SPECT_RADIUS 25 -a N 2e5 -j 1 -o output
-
-#Wait Gate pids
-processId=($(ps -ef | grep 'Gate' | grep -v 'grep' | awk '{ print $2 }'))
-for pid in ${processId[*]}; do
-  while [[ ${?} == 0 ]]; do
-    sleep 1s
-    ps -p $pid >/dev/null
-  done
-done
-
-ls
-ls output
-cat output/gate.o_0
-
-gate_power_merge.sh output
-rm -rf output
-mv results output
+Gate mac/main_arf_v2.mac
 
