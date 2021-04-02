877083e8018d1e5c90d9765579484dcdeba82b5f
Andreas Resch <resch.andi@gmail.com>
2021-04-02 09:01:46 +0200

    limited gamma index eval up to hard coded 160 mm decreased gamma tolerance level from 3mm3percent to 1mm1percent added return values

--- t8_LETActor/runAnalysis.py
+++ t8_LETActor/runAnalysis.py
@@ -35,12 +35,10 @@ def analyse_command_line(output_folders, **kwargs):
     gt.logging_conf(**kwargs)
     # Run the analysis with the command line (click)
     # the return code is 0 (fail) or 1 (success)
-    r_proton = analyse_all_folders(output_folders, 'proton')
-    r_carbon = analyse_all_folders(output_folders, 'carbon')
-    print(f'Last test return is: proton {r_proton} and carbon {r_carbon}')
+    r = analyse_all_folders(output_folders)
+    return r
 
-
-def plot_edep(filename, a):
+def plot_edep(filename, a, subType):
     # read the image
     logger.info('Load ' + filename)
     img = itk.imread(filename)
@@ -49,10 +47,17 @@ def plot_edep(filename, a):
     y = data[:, 0, 0]
     x = np.arange(len(y)) * spacing[2]
     a.plot(x, y, label=filename)
+    a.set_xlabel('Depth [mm]')
+    a.set_ylabel( subType )
     a.legend()
 
+def analyse_all_folders(output_folders):
+    r_proton = analyse_one_particle(output_folders, 'proton')
+    r_carbon = analyse_one_particle(output_folders, 'carbon')
+    print(f'Last test return is: proton {r_proton} and carbon {r_carbon}')
+    return r_proton & r_carbon
 
-def analyse_all_folders(output_folders, particle_name):
+def analyse_one_particle(output_folders, particle_name):
     # init the plot: 3 rows for gamma/proton/carbon
     fig, ax = plt.subplots(ncols=1, nrows=3, figsize=(25, 10))
     # compare with the previous folder
@@ -83,25 +88,38 @@ def gamma_index(a, filename, ref_filename):
     data = itk.GetArrayViewFromImage(gi)
     y = data[:, 0, 0]
     x = np.arange(len(y)) * spacing[2]
+ 
+    L1 =x<= 160;
+#    L1 =np.where( x<= 155);
+    ytemp = np.array(y)
+    y2 = ytemp[L1]
+    x2 = x[L1]
+    
+    
     # total
-    max = y.max()
+    max = y2.max()
     print(f'Max gamma index {ref_filename} {filename}: {max}')
     # get shared axis if already exist
     ax = a.get_shared_x_axes().get_siblings(a)[0]
     if ax == a:
         ax = a.twinx()
-    ax.plot(x, y, '--', alpha=0.5, label=f'G.I. {ref_filename} vs {filename} max={max:.2f}')
+    ax.plot(x2, y2, '--', alpha=0.5, label=f'G.I. {ref_filename} vs {filename} max={max:.2f}')
+    ax.set_ylabel( 'Gamma' )
     ax.legend()
     if max > TOL:
         return False
     return True
 
 
-def analyse_one_curve(ax, i, folder, previous_folder, filename):
+def analyse_one_curve(ax, i, folder, previous_folder, filename, subType):
     a = phsp.fig_get_sub_fig(ax, i)
+    filename = filename + '-' + subType + '.mhd'
     f = os.path.join(folder, filename)
-    plot_edep(f, a)
-    if previous_folder:
+    if (subType != 'Edep'):
+        plot_edep(f, a, subType + ' [MeV]')
+    else:
+        plot_edep(f, a, 'LET ' + subType + ' [keV/um]')
+    if previous_folder and (subType != 'Edep'):
         print('Found previous folder:', previous_folder)
         previous_filename = os.path.join(previous_folder, filename)
         r = gamma_index(a, f, previous_filename)
@@ -111,13 +129,13 @@ def analyse_one_curve(ax, i, folder, previous_folder, filename):
 
 def analyse_one_folder(ax, folder, previous_folder, particle_name):
     # plot and gi
-    rg = analyse_one_curve(ax, 0, folder, previous_folder, 'IDD-' + particle_name + '-Edep.mhd')
-    rp = analyse_one_curve(ax, 1, folder, previous_folder, 'LET_primaries-' + particle_name + '-doseAveraged.mhd')
-    rc = analyse_one_curve(ax, 2, folder, previous_folder, 'LET_primaries-' + particle_name + '-trackAveraged.mhd')
+    rg = analyse_one_curve(ax, 0, folder, previous_folder, 'IDD-' + particle_name , 'Edep')
+    rp = analyse_one_curve(ax, 1, folder, previous_folder, 'LET_primaries-' + particle_name , 'doseAveraged')
+    rc = analyse_one_curve(ax, 2, folder, previous_folder, 'LET_primaries-' + particle_name ,'trackAveraged')
     # return error if one is failed
-    # if not rg or not rp or not rc :
-        # return False
-    return True
+#    if not rg or not rp or not rc :
+#        return False
+    return rp & rc
 
 
 # -----------------------------------------------------------------------------
