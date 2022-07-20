61836d32dbbc647d8fb64180047061822ce934d0
anetxe <ane.etxebeste@creatis.insa-lyon.fr>
2022-07-20 14:00:08 +0200

    test for offline coincidences was added

--- t25_offline_digit/test_main1.py
+++ t25_offline_digit/test_main1.py
@@ -20,7 +20,7 @@ tols[4] = 0.2  # globalPosZ
 is_ok = gam.compare_root3(root_ref, root_offline,
                           "Singles", "Singles",
                           checked_keys, checked_keys,
-                          tols, scalings, scalings,
+                          tols, scalings,
                           'output/main1_root_singles.png')
 print(is_ok)
 
@@ -28,13 +28,13 @@ print(is_ok)
 # ------------------------------------------------------
 # not yet possible to compare
 
-'''print()
+print()
 gam.warning('Compare root coinc')
 root_ref = 'output/output1.root'
 root_offline = 'output/output1-coinc.root'
 
-checked_keys = ['time', 'energy',
-                'globalPosX', 'globalPosY', 'globalPosZ']
+checked_keys = ['time1', 'energy1',
+                'globalPosX1', 'globalPosY1', 'globalPosZ1']
 scalings = [1] * len(checked_keys)
 tols = [0.01] * len(checked_keys)
 tols[0] = 0.02  # time
@@ -47,6 +47,6 @@ print(scalings, tols)
 is_ok = gam.compare_root3(root_ref, root_offline,
                           "Coincidences", "Coincidences",
                           checked_keys, checked_keys,
-                          tols, scalings, scalings,
-                          'output/main1_root_singles.png')
-print(is_ok)'''
+                          tols, scalings, 
+                          'output/main1_root_coincidences.png')
+print(is_ok)
